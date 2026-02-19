648b70615f27b7118dda20fb59590b9b70be04ec
Mrxiaoyuer <a1021843147@qq.com>
2026-02-19 11:14:18 -0600

    working version after testing

--- run_5_image_comparison.sh
+++ run_5_image_comparison.sh
@@ -25,3 +25,24 @@
 #   --image ../sam3/data/eaton_palisades_30cm_4snap_temporal_refined_v1/test/images/palisades_post_2025_07_12_65408_36736.png \
 #   --prompt "building with no damage" \
 #   --output predictions.png
+
+
+
+# python3 infer_sam.py \
+#   --config configs/light_lora_config.yaml \
+#   --image ../sam3/data/eaton_palisades_30cm_4snap_temporal_refined_v1/test/images/palisades_post_2025_07_12_65408_36736.png \
+#   --prompt "building with no damage" \
+#   --output predictions.png
+
+# python3 infer_sam.py \
+#   --config configs/light_lora_config.yaml \
+#   --image ../sam3/data/eaton_palisades_30cm_4snap_temporal_refined_v1/test/images/palisades_post_2025_07_12_65408_36736.png \
+#   --prompt "debris_cleared" \
+#   --output predictions.png
+
+
+python3 infer_sam.py \
+  --config configs/light_lora_config.yaml \
+  --image ../sam3/data/eaton_palisades_30cm_4snap_temporal_refined_v1/test/images/palisades_very_post_2025_10_28_60928_34048.png \
+  --prompt "debris_cleared" \
+  --output predictions.png
